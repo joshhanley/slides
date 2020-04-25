9da290e1892d8ba84b45473d23a2a6efa215318d
Josh Hanley <josh@hitsystems.com.au>
2020-04-25 10:31:43 +1000

    Pull in tailwind and configure webpack for css export and js/css minification

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,38 @@
 let path = require('path')
+let MiniCssExtractPlugin = require("mini-css-extract-plugin");
+let OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+let TerserJSPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        app: [
+            './src/app.js',
+            './src/app.css'
+        ]
+    },
+
     output: {
-        filename: 'main.js',
+        filename: '[name].js',
         path: path.resolve(__dirname, './dist')
     },
+
+    optimization: {
+        minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"]
+            }
+        ]
+    },
+
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
+    ]
+
 }
